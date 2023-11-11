b362e269e1903a2bb61e54ec0f88ed8fab4421e4
sokolov pavel <sokolovpavel7@gmail.com>
2023-11-11 20:14:40 +0300

    section destination v2

--- js/main.js
+++ js/main.js
@@ -1,9 +1,16 @@
-$(document).ready(function () {
-    $('.owl-carousel').owlCarousel({
-        center: true,
-        loop: true,
-        margin: 30,
-        startPosition: 1, //если луп не включен, то эта настройка поставить центральную карточку на вторую позицию
-        item: 3, //три слайда на экране
-    });
+const owl = $('.owl-carousel');
+owl.owlCarousel({
+    center: true,
+    loop: true,
+    margin: 30,
+    startPosition: 1, //если луп не включен, то эта настройка поставить центральную карточку на вторую позицию
+    item: 3, //три слайда на экране
+});
+
+$('.slider__btn--prev').click(function () {
+    owl.trigger('prev.owl.carousel');
+});
+
+$('.slider__btn--next').click(function () {
+    owl.trigger('next.owl.carousel');
 });
