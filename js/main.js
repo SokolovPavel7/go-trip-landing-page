461946bdd8b208162cd23e27be35949b7abc835d
sokolov pavel <sokolovpavel7@gmail.com>
2023-11-17 04:22:33 +0300

    section destinations adaptive

--- js/main.js
+++ js/main.js
@@ -3,9 +3,22 @@ const owl = $('.owl-carousel');
 owl.owlCarousel({
     center: true,
     loop: true,
-    margin: 30,
+    margin: 20,
     startPosition: 1, //если луп не включен, то эта настройка поставить центральную карточку на вторую позицию
     item: 3, //три слайда на экране
+    responsive: {
+        850: {
+            startPosition: 1,
+            item: 3,
+        },
+        1000: {
+            margin: 20,
+            item: 3,
+        },
+        1200: {
+            margin: 30,
+        },
+    },
 });
 
 $('.slider__btn--prev').click(function () {
